7ffc412c0d4b36eacf36721a18fb8022927fea87
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-04-12 21:57:12 +0530

    Typescript config

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import clickActionReducer from "../stateSlice/clickActionSlice";
+import registerWithEmailReducer from "../slice/authSlice/RegisterWithEmailSlice"
 const store = configureStore({
   reducer: {
     clickAction: clickActionReducer,
+    registerEmail: registerWithEmailReducer
   },
 });
-
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 export default store;
