c6b6da262480fd357dca83d3532186e9442314bd
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-04-12 21:56:48 +0530

    Create Slice and action for Register with Email

--- src/redux/actions/authAction/RegisterWithEmailAction.ts
+++ src/redux/actions/authAction/RegisterWithEmailAction.ts
@@ -1,1 +1,33 @@
-export {}
+
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { RegisterWithEmailPayload } from '../../../constants/types/auth'
+import { axiosInstance } from '../../../api/axios/axiosInstance'
+import { REGISTER_EMAIL } from '../../../constants/api'
+
+
+export const registerWithEmailAction = createAsyncThunk(
+  'auth/register',
+  async ({email, password, registration_method }: RegisterWithEmailPayload , { rejectWithValue }) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      const {data}= await axiosInstance.post(
+        REGISTER_EMAIL,
+        { email, password, registration_method},
+        config
+      )
+      return data
+    } catch (error:any) {
+      console.log(error.response.data.message)
+    // return custom error message from backend if present
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message)
+      } else {
+        return rejectWithValue(error.message)
+      }
+    }
+  }   
+)
--- src/redux/slice/authSlice/RegisterWithEmailSlice.ts
+++ src/redux/slice/authSlice/RegisterWithEmailSlice.ts
@@ -1,1 +1,36 @@
-export {}
+// features/auth/authSlice.js
+import { createSlice } from '@reduxjs/toolkit'
+import { RegisterWithEmailInitialState } from '../../../constants/types/auth'
+import { registerWithEmailAction } from '../../actions/authAction/RegisterWithEmailAction'
+
+const initialState:RegisterWithEmailInitialState = {
+  loading: false,
+  userInfo: {}, // for user object
+  error: null,
+  success: false, // for monitoring the registration process.
+}
+
+const registerWithEmailSlice = createSlice({
+  name: 'registerEmail',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(registerWithEmailAction.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(registerWithEmailAction.fulfilled, (state, action) => {
+      console.log(action)
+      state.loading = false;
+      state.success= true;
+      state.userInfo = action.payload;
+    });
+    builder.addCase(registerWithEmailAction.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload
+      state.success= false;
+    });
+  },
+})
+
+export default registerWithEmailSlice.reducer
