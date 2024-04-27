c19363d70cbd7f64980cc7a1c04bb8ead43a4390
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-04-27 22:48:47 +0530

    Add more end points

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,4 @@
 export const REGISTER_EMAIL="api/users/register/"
 export const LOGIN = "/api/users/login/"
+export const SEND_OTP = "api/users/send-otp/"
+export const VERIFY_OTP= "api/users/verify-otp/"
