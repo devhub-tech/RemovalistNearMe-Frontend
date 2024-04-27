8ef1c65b22e05acdf500bdcb4f2e461e1620c5bd
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-04-27 22:48:36 +0530

    Create Components

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppDispatch, RootState } from "../redux/store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { ClipLoader } from "react-spinners";
 function SignInForm() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { error, loading, success, token, userInfo } = useSelector(
+    (state: RootState) => state.login
+  );
+
   const [state, setState] = React.useState({
     email: "",
     password: "",
+  });
+  const [errors, setErrors] = React.useState({
+    emailError: "",
+    passwordError: "",
   });
   const handleChange = (evt) => {
     const value = evt.target.value;
@@ -12,17 +24,13 @@ function SignInForm() {
     });
   };
 
-  const handleOnSubmit = (evt) => {
+  const handleOnSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-
-    const { email, password } = state;
-    alert(`You are login with email: ${email} and password: ${password}`);
-
-    for (const key in state) {
-      setState({
-        ...state,
-        [key]: "",
-      });
+    if (!state.email || !state.password) {
+      setErrors((prevState) => ({
+        emailError: state.email ? "" : "Email cannot be empty",
+        passwordError: state.password ? "" : "Password cannot be empty",
+      }));
     }
   };
 
@@ -42,26 +50,47 @@ function SignInForm() {
           </a>
         </div> */}
         <span className="span_auth">or use your account</span>
+
         <input
-          className="auth_input"
+          className={`auth_input ${errors.emailError}`}
           type="email"
-          placeholder="Email"
+          placeholder="Enter email or phone number"
           name="email"
           value={state.email}
           onChange={handleChange}
         />
+        {errors.emailError ? (
+          <span className="span_auth error_auth">{errors.emailError}</span>
+        ) : (
+          ""
+        )}
         <input
           className="auth_input"
           type="password"
           name="password"
-          placeholder="Password"
+          placeholder="Enter password"
           value={state.password}
           onChange={handleChange}
         />
+        {errors.passwordError ? (
+          <span className="span_auth error_auth">{errors.passwordError}</span>
+        ) : (
+          ""
+        )}
         <a className="a_auth" href="#">
           Forgot your password?
         </a>
-        <button className="auth_btn">Sign In</button>
+        <button className="auth_btn">
+          {loading ? (
+            <ClipLoader
+              size={20}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          ) : (
+            "SIGN IN"
+          )}
+        </button>
       </form>
     </div>
   );
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppDispatch, RootState } from "../redux/store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { SendOtpAction } from "../redux/actions/authAction/SendOtpAction";
+import { SendOtpPayload } from "../constants/types/auth";
 function SignUpForm() {
-  const [state, setState] = React.useState({
-    name: "",
+  const dispatch = useDispatch<AppDispatch>();
+  const { error, loading, success, data } = useSelector(
+    (state: RootState) => state.sendOtp
+  );
+
+  console.log(error, loading, success, data);
+  const [isVerified, setIsVerified] = useState<boolean>(false);
+  const [state, setState] = useState({
+    phone: "",
     email: "",
     password: "",
+    otp1: "",
+    otp2: "",
+    otp3: "",
+    otp4: "",
+    otp5: "",
+    otp6: "",
   });
+
   const handleChange = (evt) => {
     const value = evt.target.value;
     setState({
@@ -15,19 +33,20 @@ function SignUpForm() {
 
   const handleOnSubmit = (evt) => {
     evt.preventDefault();
-
-    const { name, email, password } = state;
-    alert(
-      `You are sign up with name: ${name} email: ${email} and password: ${password}`
-    );
-
-    for (const key in state) {
-      setState({
-        ...state,
-        [key]: "",
-      });
-    }
   };
+
+  function handleSendOtp() {
+    if (state.phone) {
+      const payload: SendOtpPayload = {
+        phone_number: state.phone,
+      };
+      dispatch(SendOtpAction(payload));
+    }
+  }
+
+  // function verfi(params:type) {
+
+  // }
 
   return (
     <div className="form-container sign-up-container">
@@ -45,31 +64,95 @@ function SignUpForm() {
           </a>
         </div> */}
         <span className="span_auth">or use your email for registration</span>
-        <input
-          className="auth_input"
-          type="text"
-          name="name"
-          value={state.name}
-          onChange={handleChange}
-          placeholder="Name"
-        />
-        <input
-          className="auth_input"
-          type="email"
-          name="email"
-          value={state.email}
-          onChange={handleChange}
-          placeholder="Email"
-        />
-        <input
-          className="auth_input"
-          type="password"
-          name="password"
-          value={state.password}
-          onChange={handleChange}
-          placeholder="Password"
-        />
-        <button className="auth_btn">Sign Up</button>
+        <div style={{ width: "100%" }}>
+          <input
+            className="auth_input"
+            type="text"
+            name="phone"
+            value={state.phone}
+            onChange={handleChange}
+            placeholder="Enter your phone number"
+          />
+          <p onClick={handleSendOtp} className="auth_verify_otp">
+            Send OTP
+          </p>
+        </div>
+
+        <div className="flex_container">
+          <input
+            className="auth_input"
+            type="text"
+            name="otp1"
+            maxLength={1}
+            value={state.otp1}
+            onChange={handleChange}
+          />
+          <input
+            className="auth_input"
+            type="text"
+            name="otp2"
+            maxLength={1}
+            value={state.otp2}
+            onChange={handleChange}
+          />
+          <input
+            className="auth_input"
+            type="text"
+            name="otp3"
+            maxLength={1}
+            value={state.otp3}
+            onChange={handleChange}
+          />
+          <input
+            className="auth_input"
+            type="text"
+            name="otp4"
+            maxLength={1}
+            value={state.otp4}
+            onChange={handleChange}
+          />
+          <input
+            className="auth_input"
+            type="text"
+            name="otp5"
+            maxLength={1}
+            value={state.otp5}
+            onChange={handleChange}
+          />
+          <input
+            className="auth_input"
+            type="text"
+            name="otp6"
+            maxLength={1}
+            value={state.otp6}
+            onChange={handleChange}
+          />
+        </div>
+
+        {isVerified && (
+          <input
+            className="auth_input"
+            type="text"
+            name="email"
+            value={state.email}
+            onChange={handleChange}
+            placeholder="Enter your email address"
+          />
+        )}
+        {isVerified && (
+          <input
+            className="auth_input"
+            type="password"
+            name="password"
+            value={state.password}
+            onChange={handleChange}
+            placeholder="Enter your password"
+          />
+        )}
+        <br />
+        <button className="auth_btn">
+          {isVerified ? "Sign Up" : "Verify OTP"}
+        </button>
       </form>
     </div>
   );
