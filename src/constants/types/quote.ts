fe35527c654b8dc16a80e90d63c60d268bf1a750
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-07-22 15:22:33 +0530

    create quote payload

---
+++ src/constants/types/quote.ts
@@ -0,0 +1,17 @@
+export interface QuotePayload {
+  user: number;
+  status?: string;
+  rooms: [];
+  additional_items: [];
+  when_type: string;
+  date_1: string;
+  date_2: string;
+  pickup_address: string;
+  pickup_address_property_type: string;
+  dropoff_address: string;
+  dropoff_address_property_type: string;
+  additional_services?: [];
+  level_of_service: string;
+  other_details?: string;
+  email: string;
+}
