7b1c3e543865de409cc6cead461a027c7705e668
Ujjwal Maurya <ujjwalsbx@gmail.com>
2024-07-22 11:28:19 +0530

    add endpoint for creating quote

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
-export const REGISTER_EMAIL="api/users/register/"
-export const LOGIN = "/api/users/login/"
-export const SEND_OTP = "api/users/send-otp/"
-export const VERIFY_OTP= "api/users/verify-otp/"
+export const REGISTER_EMAIL = "api/users/register/";
+export const LOGIN = "/api/users/login/";
+export const SEND_OTP = "api/users/send-otp/";
+export const VERIFY_OTP = "api/users/verify-otp/";
+export const CREATE_QUOTE = "/api/quotes/";
